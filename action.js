58030a385b7c4a7c369e521a994aabc5609c007d
Abhijithkm4 <84279377+Abhijithkm4@users.noreply.github.com>
2021-05-21 13:08:14 +0530

    Add files via upload

---
+++ action.js
@@ -0,0 +1,19 @@
+function yellow_btn(){
+    document.body.style.background="yellow"
+
+
+
+
+}
+
+function blue_btn(){
+    document.body.style.background="blue"
+
+
+}
+
+function red_btn(){
+    document.body.style.background="red"
+
+
+}
